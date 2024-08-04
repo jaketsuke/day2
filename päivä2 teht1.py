3f55eac37a2a386c0857299789e0cf42fcacbd89
Jaketsuke <jarkkosanteri@gmail.com>
2024-08-04 21:19:17 +0300

    Cleaned up

--- päivä2 teht1.py
+++
@@ -1,9 +0,0 @@
-# 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
-
-####################################
-#Write your code below this line 👇
-luku1 = int(two_digit_number[0])
-luku2 = int(two_digit_number[1])
-print(luku1 + luku2)
